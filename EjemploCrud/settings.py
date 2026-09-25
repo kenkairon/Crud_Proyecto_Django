54339f11a730e294e289e1590c874a2b7a00a464
kenkairon <ingeinfo2006@gmail.com>
2026-09-25 13:05:55 -0300

    Cambio de dominio

--- EjemploCrud/settings.py
+++ EjemploCrud/settings.py
@@ -24,7 +24,8 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
     "ALLOWED_HOSTS",
-    default="127.0.0.1,localhost"
+    default="127.0.0.1,localhost,.vercel.app",
+    
 ).split(",")
 
 # Application definition
